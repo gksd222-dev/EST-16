0916282b9794c6b234e23e341444db8a81334067
AhnKC <gksd222@naver.com>
2026-09-21 18:19:16 +0900

    프로젝트 수정

---
+++ JS/EST16-core-js/client/chapter/core/2.variables.js
@@ -0,0 +1,52 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin;
+let nickName = 'tiger';
+
+admin = nickName;
+
+
+const OUR_PLANET_NAME = 'earth';
+let currentOnlineUserName = 'beom';
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let calcProductPrice;
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+
+// - 구매 결제 여부
+let isPayment = true;
+let hadCartItem = false;
+
+// - 구매 결제 내역
+let paymentList;
+
+// - 오늘의 운세
+let fortuneToday = 'so good'
+
+// - 상품 정보
+let product_info = ''
+
+/* variables ----------------------------------------------------------- */
+
+
+
+/* constant variables -------------------------------------------------- */
+
+
+
+// - 1년 기준 일(day)자 수
+const DAY_PER_YEAR = 365;
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'nike';
+
+const productA ={
+    name:'',
+    price:'',
+}
---
+++ JS/EST16-core-js/server/index.js
@@ -0,0 +1,17 @@
+
+
+// console.log(process.memoryUsage()
+// );
+
+
+const server = require('live-server');
+
+const params = {
+  host:'localhost',
+  port:3000,
+  open:false,
+  root:'./client'
+}
+
+
+server.start(params);
