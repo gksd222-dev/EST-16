e010c6751e26c36cef779fb558795f6211bcfc82
AhnKC <gksd222@naver.com>
2026-09-23 17:53:53 +0900

    09/23 커밋입니다.

---
+++ JS/EST16-core-js/client/chapter/core/10. condition-1.js
@@ -0,0 +1,73 @@
+/* ---------------- */
+/* Condition        */
+/* ---------------- */
+
+
+// const result = prompt("자바스크립트의 '공식' 이름은 무엇일까요?", '');
+
+// if (result == "ECMAScript") {
+//   console.log('정답입니다!');
+// } else {
+//   console.log('모르셨나요? 정답은 ECMAScript입니다!');
+// }
+
+
+// 그 영화 봤니?
+//     ↓
+// Yes | No
+//     | 영화 볼거니?
+//           ↓
+//       Yes | No
+
+// // 영화 봤니?
+// let didWatchMovie = confirm('너 디지몬 극장판 봤니??');
+
+// if(didWatchMovie){
+//   console.log('너 좀치네?')
+
+// }else{
+//   let goingToWatchMovie = confirm('오메가몬 진화하는거 볼래?');
+
+//   if(goingToWatchMovie){
+//     let withWho = prompt('누구랑 볼거야?');
+
+//     if(withWho === '너'){
+//       console.log('사랑해');
+//     }else if(withWho == '철수'){
+//       console.log('잘가.');
+//     }else{
+//       console.log('넌 항상 그런식이야');
+//     }
+//   }
+// }
+
+
+
+
+
+// 영화 볼거니?
+   
+// if 문(statement)
+
+// else 절(clause)
+
+// else if 복수 조건 처리
+
+// 조건부 연산자
+
+// 멀티 조건부 연산자 식
+
+// 간단한 삼항식
+
+let didWatchMovie = 'no';
+let goingToWatchMovie  = 'yes';
+
+// 간단한 삼항식
+
+const message = didWatchMovie.includes('no') ? '영화 재밌더라! 한번 봐바!':
+                goingToWatchMovie.includes('yes') ? '언제 볼까? 재밌겠다!' : '그래 잘가 ㅂ_ㅇ'
+
+
+
+
+
--- JS/EST16-core-js/client/chapter/core/7.typeConversion.js
+++ JS/EST16-core-js/client/chapter/core/7.typeConversion.js
@@ -2,46 +2,46 @@
 /* Type Conversion       */
 /* --------------------- */
 
-//문자형으로 변환
-let value = true;
-console.log(typeof value);
-
-value = String(value);
-console.log(typeof value);
-
-//숫자형으로 변환
-console.log('6' / '2');
-
-let str = '123';
-console.log(typeof str);
-
-let num = Number(str); // 문자열 "123"이 숫자 123으로 변환됩니다.
-
-console.log(typeof num); //number
-
-let age = Number('임의의 문자열 123');
-
-console.log(age);
-
-console.log(Number(true));
-
-console.log(Number(false));
-
-// 불린형으로 변환
-
-const person = {
-    name: "홍길동",
-    age: 32
-};
-
-function Person(name, age) {
-    this.name = name;
-    this.age = age;
-}
-
-const person1 = new Person("홍길동", 32);
-const person2 = new Person("김철수", 25);
-
+/* //문자형으로 변환 */
+/* let value = true; */
+/* console.log(typeof value); */
+/*  */
+/* value = String(value); */
+/* console.log(typeof value); */
+/*  */
+/* //숫자형으로 변환 */
+/* console.log('6' / '2'); */
+/*  */
+/* let str = '123'; */
+/* console.log(typeof str); */
+/*  */
+/* let num = Number(str); // 문자열 "123"이 숫자 123으로 변환됩니다. */
+/*  */
+/* console.log(typeof num); //number */
+/*  */
+/* let age = Number('임의의 문자열 123'); */
+/*  */
+/* console.log(age); */
+/*  */
+/* console.log(Number(true)); */
+/*  */
+/* console.log(Number(false)); */
+/*  */
+/* // 불린형으로 변환 */
+/*  */
+/* const person = { */
+/*     name: "홍길동", */
+/*     age: 32 */
+/* }; */
+/*  */
+/* function Person(name, age) { */
+/*     this.name = name; */
+/*     this.age = age; */
+/* } */
+/*  */
+/* const person1 = new Person("홍길동", 32); */
+/* const person2 = new Person("김철수", 25); */
+/*  */
 
 /* 데이터 → 문자 ----------------------------------------------------------- */
 // number
@@ -66,6 +66,35 @@ const isClicked = false;
 /* 데이터 → 숫자 ----------------------------------------------------------- */
 
 // undefined
+const aa = {
+    name:'tiger',
+    age:30
+};
+
+//null
+const money = null;
+
+console.log(money*1);
+console.log(money/1);
+console.log(+money);
+
+//boolean
+let isActive = false;
+
+console.log(isActive/1);
+
+//string
+let num = "100";
+
+console.log(num*1);
+
+//numeric string
+const width = '120.5px';
+
+console.log( parseInt(width) );
+console.log( parseFloat(width)+ 10 + 'px');
+
+//console.log(Number(friend));
 
 // null
 
@@ -78,4 +107,21 @@ const isClicked = false;
 /* 데이터 → 불리언 ---------------------------------------------------------- */
 
 // null, undefined, 0, NaN, ''
+
+console.clear()
+
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(0));
+console.log(Boolean(NaN));
+console.log(Boolean(''));
+
 // 위에 나열한 것 이외의 것들
+
+console.log(Boolean('0'));
+console.log(Boolean(' '));
+console.log(!!(-1));
+console.log(!!({}));
+console.log(!!([false]));
+console.log(!!(()=>false));
+
